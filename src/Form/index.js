5027e1771e1b1cfcb04d077c1eec7a6c57a14a3e
Igor Kozlowski <igor3648@gmail.com>
2022-11-19 20:43:52 +0100

    task list without interactions yet

---
+++ src/Form/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Form = () => (
+    <form className="form">
+        <input className="form__input" autofocus placeholder="Jakie zadanie masz do zrobienia?" required
+            name="input" />
+        <button className="button">Dodaj zadanie</button>
+    </form>
+)
+
+export default Form;
